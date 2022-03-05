8399d168f43a7aedd4056693f89f6fabec153b0b
tvao <thorolesen14@gmail.com>
2022-03-05 16:25:05 +0000

    Add lambdas and tests

---
+++ 2 - ML Workflows/project/Lambda.py
@@ -0,0 +1,31 @@
+
+import json
+import sagemaker
+import base64
+from sagemaker.serializers import IdentitySerializer
+from sagemaker.predictor import Predictor 
+
+# Name of deployed model
+ENDPOINT = "image-classification-2022-03-05-14-01-41-157"
+
+def lambda_handler(event, context):
+    """A function to invoke model endpoint and return prediction"""
+
+    # Decode the image data
+    image = base64.b64decode(event['image_data'])
+
+    # Instantiate a Predictor
+    predictor = Predictor(ENDPOINT)
+
+    # For this model the IdentitySerializer needs to be "image/png"
+    predictor.serializer = IdentitySerializer("image/png")
+    
+    # Make a prediction:
+    inferences = predictor.predict(data=image)
+    
+    # We return the data back to the Step Function    
+    event["inferences"] = inferences.decode('utf-8')
+    return {
+        'statusCode': 200,
+        'body': json.dumps(event)
+    }
