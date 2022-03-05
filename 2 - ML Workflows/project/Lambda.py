6f6478af4f9878057aace6bd3cf39abad17c3539
tvao <thorolesen14@gmail.com>
2022-03-05 16:28:49 +0000

    Add lambda

--- 2 - ML Workflows/project/Lambda.py
+++ 2 - ML Workflows/project/Lambda.py
@@ -1,3 +1,34 @@
+import json
+import boto3
+import base64
+
+s3 = boto3.resource('s3') # Using client does not give access to bucket 
+
+def lambda_handler(event, context):
+    """A function to serialize target data from S3"""
+    
+    # Get the s3 address from the Step Function event input
+    key = event["s3_key"] 
+    bucket = event['s3_bucket']
+    
+    # Download the data from s3 to /tmp/image.png
+    s3.Bucket(bucket).download_file(key, '/tmp/image.png')
+    
+    # We read the data from a file
+    with open("/tmp/image.png", "rb") as f:
+        image_data = base64.b64encode(f.read())
+
+    # Pass the data back to the Step Function
+    print("Event:", event.keys())
+    return {
+        'statusCode': 200,
+        'body': {
+            "image_data": image_data,
+            "s3_bucket": bucket,
+            "s3_key": key,
+            "inferences": []
+        }
+    }
 
 import json
 import sagemaker
@@ -5,7 +36,7 @@ import base64
 from sagemaker.serializers import IdentitySerializer
 from sagemaker.predictor import Predictor 
 
-# Name of deployed model
+# Fill this in with the name of your deployed model
 ENDPOINT = "image-classification-2022-03-05-14-01-41-157"
 
 def lambda_handler(event, context):
@@ -25,6 +56,30 @@ def lambda_handler(event, context):
     
     # We return the data back to the Step Function    
     event["inferences"] = inferences.decode('utf-8')
+    return {
+        'statusCode': 200,
+        'body': json.dumps(event)
+    }
+
+import json
+
+THRESHOLD = .93
+
+def lambda_handler(event, context):
+    
+    # Grab the inferences from the event
+    inferences = evemt['inferences']
+    
+    # Check if any values in our inferences are above THRESHOLD
+    meets_threshold = any (inference >= THRESHOLD for inference in inferences)
+    
+    # If our threshold is met, pass our data back out of the
+    # Step Function, else, end the Step Function with an error
+    if meets_threshold:
+        pass
+    else:
+        raise("THRESHOLD_CONFIDENCE_NOT_MET")
+
     return {
         'statusCode': 200,
         'body': json.dumps(event)
